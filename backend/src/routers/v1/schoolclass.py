8511c36c1e88114b42603c07edf542d8528f23d8
Reoshiko <reoshiko@gmail.com>
2026-08-13 20:19:56 +0300

    feat: class router

---
+++ backend/src/routers/v1/schoolclass.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models.dto.schoolclass import SchoolClassResponse, SchoolClassCreate
+from src.core.database import get_session
+from src.dependencies import get_schoolclassservice
+
+router = APIRouter(prefix="/classes", tags=["classes"])
+
+
+@router.post("/", response_model=SchoolClassResponse)
+async def create(
+    payload: SchoolClassCreate,
+    session: AsyncSession = Depends(get_session),
+    repo=Depends(get_schoolclassservice),
+):
+    return await repo.create(payload=payload, session=session)
+
+
+@router.get("/", response_model=list[SchoolClassResponse])
+async def get_all(
+    session: AsyncSession = Depends(get_session), repo=Depends(get_schoolclassservice)
+):
+    return await repo.get_all(session=session)
+
+
+@router.get("/{id}", response_model=SchoolClassResponse)
+async def retrieve(
+    id: int,
+    session: AsyncSession = Depends(get_session),
+    repo=Depends(get_schoolclassservice),
+):
+    return await repo.retrieve(id=id, session=session)
