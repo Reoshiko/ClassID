23029326a21f762326f5dd3fee4252dccf1fda39
Reoshiko <reoshiko@gmail.com>
2026-08-13 22:37:41 +0300

    refactor: delete qr service

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,6 +1,5 @@
 from src.services.schoolclass import SchoolClassService
 from src.services.student import StudentService
-from src.services.qr import QRService
 
 
 def get_schoolclassservice():
@@ -9,7 +8,3 @@ def get_schoolclassservice():
 
 def get_studentservice():
     return StudentService()
-
-
-def get_qrservice():
-    return QRService()
