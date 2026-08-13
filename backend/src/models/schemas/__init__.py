634949afff16499b191e3451338ca4f285956004
Reoshiko <reoshiko@gmail.com>
2026-08-13 19:11:12 +0300

    feat: models init file

---
+++ backend/src/models/schemas/__init__.py
@@ -0,0 +1,6 @@
+from .base import Base
+from .schoolclass import SchoolClass
+from .student import Student
+from .attendance import AttendanceEvent
+
+__all__ = ["SchoolClass", "Student", "AttendanceEvent"]
