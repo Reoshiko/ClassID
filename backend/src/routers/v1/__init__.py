b8ba3ff1c22d0d98a2030f4da53dabacc2ee1a28
Reoshiko <reoshiko@gmail.com>
2026-08-13 21:33:42 +0300

    feat: add scan router to api router

--- backend/src/routers/v1/__init__.py
+++ backend/src/routers/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .schoolclass import router as SchoolclassRouter
 from .student import router as StudentRouter
+from .scanner import router as QRScannerRouter
 
 api_router = APIRouter()
 
 api_router.include_router(SchoolclassRouter, prefix="/classes", tags=["classes"])
 api_router.include_router(StudentRouter, prefix="/students", tags=["students"])
+api_router.include_router(QRScannerRouter, prefix="/scanner", tags=["scanner"])
