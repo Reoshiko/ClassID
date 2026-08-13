8b4429d522535dec735986e88448331819603174
Reoshiko <reoshiko@gmail.com>
2026-08-13 23:46:38 +0300

    feat: add attendance router

--- backend/src/routers/v1/__init__.py
+++ backend/src/routers/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from .schoolclass import router as SchoolclassRouter
 from .student import router as StudentRouter
 from .scanner import router as QRScannerRouter
+from .attendance import router as AttendanceRouter
 
 api_router = APIRouter()
 
 api_router.include_router(SchoolclassRouter, prefix="/classes", tags=["classes"])
 api_router.include_router(StudentRouter, prefix="/students", tags=["students"])
 api_router.include_router(QRScannerRouter, prefix="/scanner", tags=["scanner"])
+api_router.include_router(AttendanceRouter, prefix="/attendance", tags=["attendance"])
