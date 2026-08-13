433c7d4271f83e55102e05d94a56d1945014ba96
Reoshiko <reoshiko@gmail.com>
2026-08-13 19:14:15 +0300

    feat: configured

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,8 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from src.models.schemas import Base, __all__
+from src.core.settings import settings
 
 from alembic import context
 
@@ -11,6 +13,7 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option("sqlalchemy.url", settings.DB_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -20,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
