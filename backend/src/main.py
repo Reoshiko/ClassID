a9f228eef6961d972b951cf4f4ef0558a13db2b8
Reoshiko <reoshiko@gmail.com>
2026-08-13 20:35:04 +0300

    feat: start point

---
+++ backend/src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from src.routers.v1 import api_router
+
+app = FastAPI(title="ClassID API", version="1.0.0")
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
