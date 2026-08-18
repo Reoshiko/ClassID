f336c0763cc5394b5ee6aa0782a966f4fa8b299e
Reoshiko <reoshiko@gmail.com>
2026-08-18 09:26:20 +0300

    feat: router func (App)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+
+export function App() {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to={""} />}/>
+          </Routes>
+        </BrowserRouter>
+    )
+}
