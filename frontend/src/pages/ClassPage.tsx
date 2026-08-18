f841e794d4cd6d91389f0b6713d47b6d222a073b
Reoshiko <reoshiko@gmail.com>
2026-08-18 14:53:00 +0300

    feat/refactor: classes create

--- frontend/src/pages/ClassPage.tsx
+++ frontend/src/pages/ClassPage.tsx
@@ -4,14 +4,37 @@ import type { SchoolClass } from "../types";
 
 export function ClassesPage() {
     const [classes, setClasses] = useState<SchoolClass[]>([])
+    const [name, setName] = useState("")
+
+    async function loadClasses() {
+        const data = await api<SchoolClass[]>("/classes/")
+        setClasses(data)
+    }
+
+    async function createClass() {
+        await api<SchoolClass>("/classes/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: name
+            })
+        })
+        setName("")
+        await loadClasses()
+    }
 
     useEffect(() => {
-        api<SchoolClass[]>("/classes/").then(setClasses)
+        loadClasses()
     }, [])
 
     return (
         <div>
             <h1>Classes</h1>
+            <input placeholder="Class name" value={name} onChange={(event) => setName(event.target.value)}/>
+            <button onClick={createClass}>Create</button>
+            <hr />
             {classes.map((schoolClass) => (
                 <div key={schoolClass.id}>
                     <p>{schoolClass.id} - {schoolClass.name}</p>
