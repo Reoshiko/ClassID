0cef5c7e3b15691c950c86d7f714d897c9ba4e54
Reoshiko <reoshiko@gmail.com>
2026-08-18 11:52:51 +0300

    refactor: navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
 
 
 export function App() {
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Navigate to={""} />}/>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/students" replace />}/>
+            </Route>
           </Routes>
         </BrowserRouter>
     )
